7e3adbd43378c8d2c3c7608eba3be35030b6b00f
jamaludeen <justjamal17@gmail.com>
2022-09-15 12:23:12 +0530

    condition

---
+++ index.js
@@ -0,0 +1,15 @@
+
+ let 
+     members=["b101","b102","b103","b104","b105","b106","b107","b108","b109","b110",],
+     result=members.length;
+ for ( var i = 0; i < result;i++){
+	 
+      console.log('door-no :'+ members[i]);
+	
+	}
+	 
+ 
+ 
+ 
+ 
+ 
